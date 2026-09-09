548bc9a80041b1a939d9182c73546c37f3c3b5d6
Mashopy <eliasgheeraert@gmail.com>
2026-09-10 01:25:40 +0200

    Passbook: Bump Gradle wrapper, AGP, dependencies and Android Studio version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,11 @@ android {
         compose = true
         buildConfig = true
     }
+    packaging {
+        resources {
+            excludes += "/META-INF/LICENSE.md"
+        }
+    }
 }
 
 dependencies {
@@ -48,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
